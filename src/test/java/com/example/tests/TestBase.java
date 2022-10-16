43d831f919989cb1cd773baa6d793bc6f9d6b595
TimeQA <qaeducation2022@gmail.com>
2022-10-16 19:33:04 +0300

    remove headlees

--- src/test/java/com/example/tests/TestBase.java
+++ src/test/java/com/example/tests/TestBase.java
@@ -13,7 +13,7 @@ public class TestBase {
     @BeforeAll
     static void configure() {
         SelenideLogger.addListener("AllureSelenide", new AllureSelenide());
-        Configuration.headless = true;
+//        Configuration.headless = true;
         Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub";
 
         DesiredCapabilities capabilities = new DesiredCapabilities();
