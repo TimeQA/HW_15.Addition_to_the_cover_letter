8a7505df144c0c3657ab98565c921c63fcb4c5f9
TimeQA <qaeducation2022@gmail.com>
2022-10-16 18:14:20 +0300

    add attachments

--- src/test/java/com/example/tests/TestBase.java
+++ src/test/java/com/example/tests/TestBase.java
@@ -2,7 +2,9 @@ package com.example.tests;
 
 import com.codeborne.selenide.Configuration;
 import com.codeborne.selenide.logevents.SelenideLogger;
+import com.example.helpers.AllureAttachments;
 import io.qameta.allure.selenide.AllureSelenide;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.openqa.selenium.chrome.ChromeOptions;
 
@@ -11,13 +13,20 @@ public class TestBase {
     static void configure() {
         SelenideLogger.addListener("AllureSelenide", new AllureSelenide());
         Configuration.headless = true;
-
         Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub";
+
         ChromeOptions chromeOptions = new ChromeOptions();
         chromeOptions.addArguments("--no-sandbox");
         chromeOptions.addArguments("--disable-infobars");
         chromeOptions.addArguments("--disable-popup-blocking");
         chromeOptions.addArguments("--disable-notifications");
         chromeOptions.addArguments("--lang=en-en");
+    }
+
+    @AfterEach
+    void addAttachments() {
+        AllureAttachments.screenshotAs("Last screenshot");
+        AllureAttachments.pageSource();
+        AllureAttachments.browserConsoleLogs();
     }
 }
